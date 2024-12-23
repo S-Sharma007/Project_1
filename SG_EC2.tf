6f3a49db9d031d3bb3f6719d5bfe4d67690c1354
S-Sharma007 <shubhamsharma95363@gmail.com>
2024-12-23 20:01:47 +0530

    V3 with security group

--- SG_EC2.tf
+++ SG_EC2.tf
@@ -3,22 +3,22 @@ provider "aws" {
 }
 
 resource "aws_instance" "EC1" {
-  ami = "ami-0ae69fd56f71576ad"
-  instance_type = "t2.micro"
-  key_name = "DevOps_complete_Project"
-  security_groups = [ SG_project1 ]
+  ami             = "ami-0ae69fd56f71576ad"
+  instance_type   = "t2.micro"
+  key_name        = "DevOps_complete_Project"
+  security_groups = [aws_security_group.SG_project1.name]
 
   tags = {
-   Name = "EC1"
+    Name = "EC1"
   }
 }
 
- /* Creation of the SG */
+/* Creation of the SG */
 
 resource "aws_security_group" "SG_project1" {
   name        = "SG_project1"
   description = "ALLow inbount traffic SSH connection"
-  
+
 
   tags = {
     Name = "SG_project1"
@@ -27,17 +27,17 @@ resource "aws_security_group" "SG_project1" {
 
   ingress {
     description = "ssh access"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-    egress {
+  egress {
     description = "ssh access"
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-}
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
